dcc897a525db66cf2c71730e5219762f5400e0ee
Demon437 <namdevshanu206@gmail.com>
2024-11-11 17:36:50 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Counter from "./COUNTER2.0/Counter";
 import Routing1 from "./Routing1";
 import Usestatefun from "./Use State Hook/Usestate fun ";
 
 function App() {
   return (
     <div>
-      <Routing1></Routing1>
-    </div>
+<Counter></Counter>  
+  </div>
   );
 }
 export default App;
